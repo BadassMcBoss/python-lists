fb796a81d873bd5081923f2924e8d42cade3d727
BadassMcBoss <jzepeda031898@gmail.com>
2024-02-25 15:52:34 -0600

    adding assignment 3

---
+++ Adv Slicing Tech/monthly_temp.py
@@ -0,0 +1,10 @@
+temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+
+temperatures_2nd = temperatures[7:14]
+print(temperatures_2nd)
+
+over_100 = temperatures[23:30]
+print(over_100)
+
+temperatures.sort(reverse = True)
+print(temperatures[4:9])
